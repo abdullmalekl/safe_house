26c942f947f9e5ab7189de7ab33be2b31ccd45f6
malek <you@example.com>
2023-08-01 20:28:28 +0200

    Add existing project files to Git

--- src/Bars/navBar.js
+++ src/Bars/navBar.js
@@ -16,7 +16,7 @@ const Navbar = ()=>{
         <nav id={styles.navBar} className="navbar navbar-expand-lg bg-body-tertiary">
   <div  className="container-fluid">
   <Link to="/" id={styles.home} className="navbar-brand"  >Home</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button id={styles.toggle} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
