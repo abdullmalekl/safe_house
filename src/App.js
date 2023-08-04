9d775db5a2f6fe54d38438242b8683070e9cd5c1
malek <you@example.com>
2023-08-04 19:27:31 +0200

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -7,17 +7,9 @@ import Navbar from './Bars/navBar';
 function App() {
   return (
     <div className="App">
-      <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <meta thhp-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      </head>
       <AppLogo />
       <Navbar />
-      {/* <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header> */}
+      
     </div>
   );
 }
